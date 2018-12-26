1b1e206fad5368129babd630d0f40930c24cff45
Emilio Marquez Fernandez <emmmarqu@ucsc.edu>
2018-12-26 12:14:05 -0800

    initalized default xcode ios files for calfit app

---
+++ calfit-ios/calfit-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calfit-ios
+//
+//  Created by Manny  on 12/26/18.
+//  Copyright © 2018 Emilio Marquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
